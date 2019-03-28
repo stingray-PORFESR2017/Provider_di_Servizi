7b12bb519c8d24d4c89626579d902db810bf7a88
Giorgio <imatesiu@gmail.com>
2019-03-28 23:56:23 +0100

    Update travisafter.sh

--- travisafter.sh
+++ travisafter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd $TRAVIS_BUILD_DIR/Provider_di_Servizi/Servizi_Utente_Ferrovia/Servizio_Supervisione_Stazione/
+cd $TRAVIS_BUILD_DIR/Servizi_Utente_Ferrovia/Servizio_Supervisione_Stazione/
 mvn jacoco:report coveralls:report
