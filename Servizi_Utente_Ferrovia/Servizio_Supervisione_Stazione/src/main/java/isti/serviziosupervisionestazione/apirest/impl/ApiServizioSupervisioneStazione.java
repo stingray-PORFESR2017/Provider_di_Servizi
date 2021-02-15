c39fc7e958d33b71e96ff9e9a8a9d33a24859e1d
Giorgio <imatesiu@gmail.com>
2021-02-15 23:25:57 +0100

    Update ApiServizioSupervisioneStazione.java

--- Servizi_Utente_Ferrovia/Servizio_Supervisione_Stazione/src/main/java/isti/serviziosupervisionestazione/apirest/impl/ApiServizioSupervisioneStazione.java
+++ Servizi_Utente_Ferrovia/Servizio_Supervisione_Stazione/src/main/java/isti/serviziosupervisionestazione/apirest/impl/ApiServizioSupervisioneStazione.java
@@ -94,7 +94,9 @@ public class ApiServizioSupervisioneStazione {
 				String responseAsString = response1.readEntity(String.class);
 				String res = responseAsString.replaceAll("background-image: url(\"../images/header_mobile.png\");","");
 				 res = res.replaceAll("http://www.viaggiatreno.it/vt_pax_internet/","https://stingray.isti.cnr.it:8443/serviziosupervisionestazione/pis/viaggiatreno/site/");
-				res  = res.replaceAll("Copyright Trenitalia S.p.A. 2006.", "");
+				 res = res.replaceAll("/vt_pax_internet/","https://stingray.isti.cnr.it:8443/serviziosupervisionestazione/pis/viaggiatreno/site/");
+				 res = res.replaceAll("../images/iconaRicerca.png","http://www.viaggiatreno.it/vt_pax_internet/images/iconaRicerca.png");
+				 res  = res.replaceAll("Copyright Trenitalia S.p.A. 2006.", "");
 				res  = res.replaceAll("Tutti i diritti riservati.", "");
 				 return res;
 				/*response.setStatus(response.SC_MOVED_TEMPORARILY);
