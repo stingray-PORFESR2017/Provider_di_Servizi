abfd76929415f875951b15f60c338bfc01ddfb6e
Giorgio <imatesiu@gmail.com>
2021-02-16 09:55:17 +0100

    Update ApiServizioSupervisioneStazione.java

--- Servizi_Utente_Ferrovia/Servizio_Supervisione_Stazione/src/main/java/isti/serviziosupervisionestazione/apirest/impl/ApiServizioSupervisioneStazione.java
+++ Servizi_Utente_Ferrovia/Servizio_Supervisione_Stazione/src/main/java/isti/serviziosupervisionestazione/apirest/impl/ApiServizioSupervisioneStazione.java
@@ -75,12 +75,76 @@ public class ApiServizioSupervisioneStazione {
 		//return "<html><body>OK</body></html>";
 	}
 	
+	@PermitAll
+	@Path("/viaggiatreno/site/{key:.*}")
+	@POST
+	public String postviaggiatrenosite(@PathParam("key") String key, 
+			@QueryParam("stazione") String stazione, 
+			@QueryParam("lang") String lang, 
+			
+			@Context HttpServletRequest request, @Context HttpServletResponse response) {
+		String path = "http://www.viaggiatreno.it/vt_pax_internet/";
+		
+		
+		
+		
+		//List<String> listparametri = new ArrayList<String>();
+		String tmp = "";
+		if(stazione!=null)
+			if(stazione!="")
+				tmp = "stazione="+stazione;
+			//listparametri.add("stazione="+stazione);
+		if(lang!=null)
+			if(tmp!="")
+				tmp = tmp+"&lang="+lang;
+			else
+				tmp = "?lang="+lang;
+			//listparametri.add("lang="+lang);
+		
+		String url  = path + key + tmp;
+
+		log.info(url+"\n\r");
+		
+		
+		String contenttype = null;
+		try {
+			if(request!=null)
+				contenttype = request.getContentType();
+			log.info(url+"\n\r");
+			
+
+				Client client = ClientBuilder.newClient();
+				if(contenttype==null)
+					contenttype = "application/json";
+				Response response1 = client.target(url).request().header("Content-Type", contenttype).get();
+				String responseAsString = response1.readEntity(String.class);
+				String res = responseAsString.replaceAll("background-image: url(\"../images/header_mobile.png\");","");
+				 res = res.replaceAll("http://www.viaggiatreno.it/vt_pax_internet/","https://stingray.isti.cnr.it:8443/serviziosupervisionestazione/pis/viaggiatreno/site/");
+				 res = res.replaceAll("/vt_pax_internet/","https://stingray.isti.cnr.it:8443/serviziosupervisionestazione/pis/viaggiatreno/site/");
+				 res = res.replaceAll("../images/iconaRicerca.png","http://www.viaggiatreno.it/vt_pax_internet/images/iconaRicerca.png");
+				 res  = res.replaceAll("Copyright Trenitalia S.p.A. 2006.", "");
+				res  = res.replaceAll("Tutti i diritti riservati.", "");
+				 return res;
+				/*response.setStatus(response.SC_MOVED_TEMPORARILY);
+				response.setHeader("Location", url);
+				response.sendRedirect(url);*/
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				log.error(e);
+			}
+			String err =  "<html><body>ERROR</body></html>";
+			return err;
+		
+		
+		
+		//return "<html><body>OK</body></html>";
+	}
 	
 	
 	@PermitAll
 	@Path("/viaggiatreno/site/{key:.*}")
 	@GET
-	@POST
 	public String viaggiatrenosite(@PathParam("key") String key, 
 			@QueryParam("stazione") String stazione, 
 			@QueryParam("lang") String lang, 
